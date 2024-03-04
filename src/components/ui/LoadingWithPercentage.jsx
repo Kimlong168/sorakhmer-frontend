a66c04c9f1f27e083d5b47d4c74df89d40098611
kimlong <kimlong5244@gmail.com>
2024-03-04 15:17:24 +0700

    fix progress bar3

--- src/components/ui/LoadingWithPercentage.jsx
+++ src/components/ui/LoadingWithPercentage.jsx
@@ -2,7 +2,11 @@ import PropTypes from "prop-types";
 const LoadingWithPercentage = ({ percentage }) => {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-48 sm:w-56 md:w-64 h-4 bg-gray-800 relative">
+      <div
+        className={`w-48 sm:w-56 md:w-64 h-4 ${
+          percentage == 100 ? "" : " bg-gray-800 "
+        } relative`}
+      >
         <div
           className="absolute top-0 left-0 h-full bg-blue-500  transition-all  ease-in-out"
           style={{ width: `${percentage}%` }}
